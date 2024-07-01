7234c97a9ba1b54a2fcf7d1f7231cb187c822af9
Leo Wylonis <leo.wylonis@yale.edu>
2024-07-01 14:40:34 -0400

    Added hyperparameters

--- bash/run_classification_nn.sh
+++ bash/run_classification_nn.sh
@@ -3,8 +3,8 @@
 export CUDA_VISIBLE_DEVICES=0
 
 python src/run_classification_nn.py \
---n_batch # TODO: Fill in hyperparameter \
---dataset # TODO: Fill in hyperparameter \
---checkpoint_path # TODO: Fill in hyperparameter \
---output_path # TODO: Fill in hyperparameter \
---device # TODO: Fill in hyperparameter \
+--n_batch 24 # TODO: Fill in hyperparameter \
+--dataset mnist # TODO: Fill in hyperparameter \
+--checkpoint_path trained_classification # TODO: Fill in hyperparameter \
+--output_path trained_classification/evaluation_results # TODO: Fill in hyperparameter \
+--device gpu # TODO: Fill in hyperparameter \
--- bash/train_classification_nn.sh
+++ bash/train_classification_nn.sh
@@ -3,11 +3,11 @@
 export CUDA_VISIBLE_DEVICES=0
 
 python src/train_classification_nn.py \
---n_batch # TODO: Fill in hyperparameter \
---dataset # TODO: Fill in hyperparameter \
---n_epoch # TODO: Fill in hyperparameter \
---learning_rate # TODO: Fill in hyperparameter \
---learning_rate_decay # TODO: Fill in hyperparameter \
---learning_rate_period # TODO: Fill in hyperparameter \
---checkpoint_path # TODO: Fill in hyperparameter \
---device # TODO: Fill in hyperparameter \
+--n_batch 24 # TODO: Fill in hyperparameter \
+--dataset mnist # TODO: Fill in hyperparameter \
+--n_epoch 50 # TODO: Fill in hyperparameter \
+--learning_rate 1e-3 # TODO: Fill in hyperparameter \
+--learning_rate_decay 0.5 # TODO: Fill in hyperparameter \
+--learning_rate_period 10 # TODO: Fill in hyperparameter \
+--checkpoint_path trained_classification # TODO: Fill in hyperparameter \
+--device gpu # TODO: Fill in hyperparameter \
